8d39afb0477f01ccbd0eaea18979c0e1afdb18a0
ize <icedza@gmail.com>
2022-10-17 16:20:45 +0700

    stat

--- app.vue
+++ app.vue
@@ -1,93 +1,150 @@
 <template>
-  <form @submit.prevent="postToDB" class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
-      <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-        <h1 class="title-font font-medium text-3xl text-gray-900">
-          Subscribe to my newsletter
-        </h1>
-        <p class="leading-relaxed mt-4">
-          I send out a weekly newsletter with tips and tricks for web
-          development. I promise, only relevant content will be sent.
-        </p>
-      </div>
-      <div
-        class="
-          lg:w-2/6
-          md:w-1/2
-          bg-gray-100
-          rounded-lg
-          p-8
-          flex flex-col
-          md:ml-auto
-          w-full
-          mt-10
-          md:mt-0
-        "
-      >
-        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
-          Subscribe
-        </h2>
-
-        <div class="relative mb-4">
-          <label for="email" class="leading-7 text-sm text-gray-600"
-            >Email</label
-          >
-          <input
-            type="email"
-            id="email"
-            v-model="userEmail"
-            name="email"
-            class="
-              w-full
-              bg-white
-              rounded
-              border border-gray-300
-              focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200
-              text-base
-              outline-none
-              text-gray-700
-              py-1
-              px-3
-              leading-8
-              transition-colors
-              duration-200
-              ease-in-out
-            "
-          />
-        </div>
-        <button
-          class="
-            text-white
-            bg-indigo-500
-            border-0
-            py-2
-            px-8
-            focus:outline-none
-            hover:bg-indigo-600
-            rounded
-            text-lg
-          "
+  <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+    <div
+      class="lg:w-2/6 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 mx-auto"
+    >
+      <div class="pow my-0.5">
+        <div
+          class="mb-0.5 bg-red-400 text-black flex flex-row text-center items-center rounded"
         >
-          Subscribe
-        </button>
-        <p class="text-xs text-gray-500 mt-3">
-          Literally you probably haven't heard of them the things i'm bout to
-          shoot into your inbox.
-        </p>
+          <div class="font-bold text-white w-4/12 mx-auto">AP</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div
+            class="items-center w-2/12 bg-white rounded mr-0.5 flex justify-between"
+          >
+            <div>x</div>
+            <div>0</div>
+            <div class="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div
+          class="mb-0.5 bg-red-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">AC</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-red-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">DX</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
       </div>
-      <pre>{{ DBResponse }}</pre>
+
+      <div class="pow my-0.5">
+        <div
+          class="mb-0.5 bg-blue-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">MP</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-blue-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">MA</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-blue-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">MD</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+      </div>
+
+      <div class="pow my-0.5">
+        <div
+          class="mb-0.5 bg-purple-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">WT</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-purple-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">DA</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-purple-400 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">LK</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+      </div>
+
+      <div class="charm my-0.5">
+        <div
+          class="mb-0.5 bg-yellow-300 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">HP</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-yellow-300 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">DP</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+        <div
+          class="mb-0.5 bg-yellow-300 text-black flex flex-row text-center items-center rounded"
+        >
+          <div class="font-bold text-white w-4/12 mx-auto">HV</div>
+          <div class="w-8/12 mx-auto bg-white rounded my-0.5">30+0</div>
+          <div class="w-2/12 bg-white rounded m-0.5">Lv.2</div>
+          <div class="w-2/12 bg-white rounded mr-0.5">3</div>
+        </div>
+      </div>
     </div>
-  </form>
+    <!-- <pre>{{ DBResponse }}</pre> -->
+  </div>
 </template>
 <script setup>
-// variable to hold the response from supabase
-const DBResponse = ref([]);
-// variable to hold the provided email address
-const userEmail = ref("");
-// function to send email to our server route
-async function postToDB() {
-  const result = await fetch(`/api/subscribe?email=${userEmail.value}`);
-  const data = await result.json();
-  DBResponse.value = data;
-}
+  // variable to hold the response from supabase
+  const DBResponse = ref([]);
+  // variable to hold the provided email address
+  const userEmail = ref("");
+  // function to send email to our server route
+
+  async function postToDB() {
+    const result = await fetch(`/api/build?`);
+    const data = await result.json();
+    DBResponse.value = data;
+  }
+
+  // await postToDB();
 </script>
